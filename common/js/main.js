776456129c76795bd779b22163f8335850753320
cafefarm-soyeon <hayehfrp@gmail.com>
2020-03-26 13:30:44 +0900

    2200326_visual add

---
+++ common/js/main.js
@@ -0,0 +1,9 @@
+$(window).scroll(function() {
+	var scroll = $(window).scrollTop();
+	//console.log(scroll);
+	if (scroll <= 800 && scroll > 400) {
+		//console.log('500')
+		//$(".wave_wrap").toggleClass("wave_warp_up");
+    }
+    
+});
