896ab8d768d285f652288060e97367af4e2cb334
cafefarm-soyeon <hayehfrp@gmail.com>
2020-03-26 20:24:58 +0900

    20203626_career

--- common/js/main.js
+++ common/js/main.js
@@ -1,9 +1,12 @@
-$(window).scroll(function() {
-	var scroll = $(window).scrollTop();
-	//console.log(scroll);
-	if (scroll <= 800 && scroll > 400) {
-		//console.log('500')
-		//$(".wave_wrap").toggleClass("wave_warp_up");
-    }
-    
-});
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+	  console.log('50');
+    //document.getElementById("header").style.fontSize = "30px";
+  } else {
+	console.log('others');
+    //document.getElementById("header").style.fontSize = "90px";
+  }
+}
+
