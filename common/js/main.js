81deb6ad7ef905a3b421ef5b4e2d3af90b18c4e3
cafefarm-soyeon <hayehfrp@gmail.com>
2020-03-27 16:30:19 +0900

    console log

--- common/js/main.js
+++ common/js/main.js
@@ -1,12 +1,1 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-	  console.log('50');
-    //document.getElementById("header").style.fontSize = "30px";
-  } else {
-	console.log('others');
-    //document.getElementById("header").style.fontSize = "90px";
-  }
-}
 
