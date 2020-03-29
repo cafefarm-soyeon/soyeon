e07d70dba8fbb97ade07d8fa1238c03d06dc1395
cafefarm-soyeon <hayehfrp@gmail.com>
2020-03-29 16:30:06 +0900

    200329 js test

--- common/js/main.js
+++ common/js/main.js
@@ -1,10 +1,12 @@
 (function(){
    // console.log(window.innerWidth);
-
+   
+   
 
     $(window).scroll(function () {
         var height = $(document).scrollTop();
         //console.log(height);
+
 
         if(height > 500){
             $('.arrow').hide();
