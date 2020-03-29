bc89b06aa8eb2e3ff305147e634eee92b1013c10
cafefarm-soyeon <hayehfrp@gmail.com>
2020-03-29 14:23:51 +0900

    200329 arrow add

--- common/js/main.js
+++ common/js/main.js
@@ -1,3 +1,17 @@
 (function(){
-    //console.log(window.innerWidth);
+   // console.log(window.innerWidth);
+
+
+    $(window).scroll(function () {
+        var height = $(document).scrollTop();
+        //console.log(height);
+
+        if(height > 500){
+            $('.arrow').hide();
+        }else{
+            $('.arrow').show();
+        }
+        }); 
+
 }());
+
